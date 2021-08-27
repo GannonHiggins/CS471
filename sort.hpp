528d95db488ee7ca0f745511bc18102043f5b15b
Gannon Higgins <akg98@me.com>
2021-08-27 12:16:46 -0800

    added basic sudocode for quicksort

--- sort.hpp
+++ sort.hpp
@@ -1,7 +1,13 @@
 
 template <typename T>
-T Quicksrt(T A, T lo, T hi){
-
+T Quicksort(T A, T lo, T hi){
+	if(lo >= 0 && hi >= 0){
+		if(lo < hi){
+			p = partition(A, lo hi)
+			quicksort(A lo, p-1)
+			quicksort(A p+1, hi)
+		}
+	}
 }
 
 template <typename B>
