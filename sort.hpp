c4ea01344b689d3ff6bd9d89e9aa1c7f3ad07617
Gannon Higgins <akg98@me.com>
2021-08-30 14:40:00 -0800

    modified comments

--- sort.hpp
+++ sort.hpp
@@ -1,13 +1,5 @@
 #include <algorithm>
 #include <iostream>
-/*
-Functions to sort Vectors of Integers
-
-Precondition:
-A =  MUST be a Vector of Integers
-lo = Mst be a Value lower than the hi value of the hi value and must be greater than zero 
-hi = Must be a values higher than lo and be greater than zero 
-*/
 
 
 int partition(int arr[], int lo, int hi){
