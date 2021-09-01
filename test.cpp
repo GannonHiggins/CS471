7f718e358efde6fdfcda73369aea2cec20eb85f4
Gannon Higgins <akg98@me.com>
2021-08-31 18:42:36 -0800

    created test case with doctest

--- test.cpp
+++ test.cpp
@@ -1,22 +1,37 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <cstdio>
 #include "doctest.h"
 #include "sort.hpp"
 
-void printArray(int arr[], int size){
-	for (int i=0;i<size; i++){
-		printf("%d ", arr[i]);
+bool CheckSorted(int arr[],int n){
+
+	if(n == 0 || n == 1) {
+		return true;
 	}
-	printf("\n");
+
+	for (int i =1; i < n; i++){
+		if(arr[i-1] > arr[i]){
+			return false;
+		}
+	}
+	return true;
 }
 
-int main() {
 
-	int arr[] = {4,2,3,1,0,5,10};
+TEST_CASE("Unsorted arrays"){
+	int arr[] = {3,4,5,6,7};
+	int n1 = sizeof(arr)/sizeof(arr[0]);
+	quicksort(arr,0,n1-1);
 
-	int n = sizeof(arr)/sizeof(arr[0]);
+	int arr1[] = {3,8,9,2,4};
+	int n2 = sizeof(arr1)/sizeof(arr1[0]);
+	quicksort(arr1,0,n2-1);
 
-	quicksort(arr,0,n-1);
-	printf("Sorted array: \n");
-	printArray(arr,n);
-	return 0; 
+	int arr2[] = {3,8,9,2,4,10};
+	int n3 = sizeof(arr2)/sizeof(arr2[0]);
+
+	CHECK(CheckSorted(arr,n1) == true);
+	CHECK(CheckSorted(arr1,n2) == true);
+	CHECK(CheckSorted(arr2,n3) == false);
+
 }
