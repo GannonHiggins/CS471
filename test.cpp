d372b602e422e6d31194260050e4556c1c747c13
Gannon Higgins <akg98@me.com>
2021-09-01 11:00:48 -0800

    modfied test cases to final product

--- test.cpp
+++ test.cpp
@@ -30,9 +30,15 @@ bool CheckSorted(int arr[],int n){
 	return true;
 }
 
+void print_array(int arr[],int n){
+	for(int i=0; i < n; i++){
+		std::cout<<arr[i]<< " ";
+	}
+}
+
 TEST_CASE("Unsorted arrays"){
 
-	int arr[] = {3,4,5,6,7};
+	int arr[] = {3,5,4,6,7,1};
 	int n1 = sizeof(arr)/size_int;
 	quicksort(arr,0,n1-1);
 
@@ -40,6 +46,8 @@ TEST_CASE("Unsorted arrays"){
 	int n2 = sizeof(arr1)/size_int;
 	quicksort(arr1,0,n2-1);
 
+	//Third test will not use quicksort
+	// to verify that the arrays are sorted properly
 	int arr2[] = {3,8,9,2,4,10};
 	int n3 = sizeof(arr2)/size_int;
 
@@ -51,8 +59,13 @@ TEST_CASE("Unsorted arrays"){
 
 TEST_CASE("Sorted Array"){
 	int arr[]= {1,2,3,4,5,6};
+	quicksort(arr,1,6);
+
 	int arr1[] = {10,50,60,70,80,90};
+	quicksort(arr1,0,sizeof(arr1)/size_int);
+
 	int arr2[] = {0,10,88,99,100};
+	quicksort(arr2,10,sizeof(arr2)/size_int);
 
 
 
@@ -64,9 +77,16 @@ TEST_CASE("Sorted Array"){
 
 TEST_CASE("Arrays of size 1"){
 	int arr[]= {1};
+	quicksort(arr,0,sizeof(arr)/size_int);
+	
 	int arr1[] = {10};
+	quicksort(arr1,0,sizeof(arr1)/size_int);
+
 	int arr2[] = {0};
+	quicksort(arr2,0,sizeof(arr2)/size_int);
+
 	int arr3[] = {100};
+	quicksort(arr3,0,sizeof(arr3)/size_int);
 
 	CHECK(CheckSorted(arr,sizeof(arr)/size_int) == true);
 	CHECK(CheckSorted(arr1,sizeof(arr)/size_int) == true);
