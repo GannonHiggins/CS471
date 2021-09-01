5f78c63d9fa69e924d4d6a2897d0b43ea0f9a2fe
Gannon Higgins <akg98@me.com>
2021-09-01 09:11:17 -0800

    added some more tests cases

--- test.cpp
+++ test.cpp
@@ -3,8 +3,19 @@
 #include "doctest.h"
 #include "sort.hpp"
 
-int size_int = sizeof(int);
+int size_int = sizeof(int); // Size of int value, ued to calculate the size of arrays
 
+/* 
+This function cheack each element at position n-1 and makes sure it is smaller than 
+position n. 
+returns True if all elements at position n-1 are smaller than n
+returns false if otherwise. 
+
+
+Preconditions: 
+	Arrays must be of Ints and must cuntain at least 1 values within the array
+	second value must be the Size of the array.
+*/
 bool CheckSorted(int arr[],int n){
 
 	if(n == 0 || n == 1) {
@@ -42,10 +53,24 @@ TEST_CASE("Sorted Array"){
 	int arr[]= {1,2,3,4,5,6};
 	int arr1[] = {10,50,60,70,80,90};
 	int arr2[] = {0,10,88,99,100};
-	int arr3[] = {100,0,5,10,3,2};
 
 
 
 	CHECK(CheckSorted(arr,sizeof(arr)/size_int) == true);
+	CHECK(CheckSorted(arr1,sizeof(arr1)/size_int) == true);
+	CHECK(CheckSorted(arr2,sizeof(arr2)/size_int) == true);
+
+}
+
+TEST_CASE("Arrays of size 1"){
+	int arr[]= {1};
+	int arr1[] = {10};
+	int arr2[] = {0};
+	int arr3[] = {100};
+
+	CHECK(CheckSorted(arr,sizeof(arr)/size_int) == true);
+	CHECK(CheckSorted(arr1,sizeof(arr)/size_int) == true);
+	CHECK(CheckSorted(arr2,sizeof(arr)/size_int) == true);
+	CHECK(CheckSorted(arr3,sizeof(arr)/size_int) == true);
 
 }
