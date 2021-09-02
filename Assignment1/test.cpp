f2c1282b8052166e562f3720f15791ea8a4e8cbe
Gannon Higgins <akg98@me.com>
2021-09-01 16:06:43 -0800

    fixed error for windows machine

--- Assignment1/test.cpp
+++ Assignment1/test.cpp
@@ -32,7 +32,11 @@ bool CheckSorted(int arr[],int n){
 	}
 	return true;
 }
-
+void print_array(int arr[],int n){
+	for(int i =0; i<n; i++){
+		std::cout<<arr[i]<<" ";
+	}
+}
 TEST_CASE("Unsorted arrays"){
 
 	int arr[] = {3,5,4,6,7,1};
@@ -47,22 +51,23 @@ TEST_CASE("Unsorted arrays"){
 	// to verify that the arrays are sorted properly
 	int arr2[] = {3,8,9,2,4,10};
 	int n3 = sizeof(arr2)/size_int;
+	quicksort(arr2,0,n3-1);
 
 	CHECK(CheckSorted(arr,n1) == true);
 	CHECK(CheckSorted(arr1,n2) == true);
-	CHECK(CheckSorted(arr2,n3) == false);
+	CHECK(CheckSorted(arr2,n3) == true);
 
 }
 
 TEST_CASE("Sorted Arrays"){
 	int arr[]= {1,2,3,4,5,6};
-	quicksort(arr,0,sizeof(arr)/size_int);
+	quicksort(arr,0,(sizeof(arr)/size_int)-1);
 
-	int arr1[] = {10,50,60,70,80,90};
-	quicksort(arr1,0,sizeof(arr1)/size_int);
+	int arr1[] = {10,50,60,70,80,90,99,100,101};
+	quicksort(arr1,0,(sizeof(arr1)/size_int)-1);
 
 	int arr2[] = {0,10,88,99,100};
-	quicksort(arr2,0,sizeof(arr2)/size_int);
+	quicksort(arr2,10,(sizeof(arr2)/size_int)-1);
 
 
 
@@ -74,21 +79,22 @@ TEST_CASE("Sorted Arrays"){
 
 TEST_CASE("Arrays of size 1"){
 	int arr[]= {1};
-	quicksort(arr,0,sizeof(arr)/size_int);
+	int array_size = sizeof(arr)/size_int;
+	quicksort(arr,0,array_size);
 	
 	int arr1[] = {10};
-	quicksort(arr1,0,sizeof(arr1)/size_int);
+	quicksort(arr1,0,array_size);
 
 	int arr2[] = {0};
-	quicksort(arr2,0,sizeof(arr2)/size_int);
+	quicksort(arr2,0,array_size);
 
 	int arr3[] = {100};
-	quicksort(arr3,0,sizeof(arr3)/size_int);
+	quicksort(arr3,0,array_size);
 
-	CHECK(CheckSorted(arr,sizeof(arr)/size_int) == true);
-	CHECK(CheckSorted(arr1,sizeof(arr)/size_int) == true);
-	CHECK(CheckSorted(arr2,sizeof(arr)/size_int) == true);
-	CHECK(CheckSorted(arr3,sizeof(arr)/size_int) == true);
+	CHECK(CheckSorted(arr,1) == true);
+	CHECK(CheckSorted(arr1,1) == true);
+	CHECK(CheckSorted(arr2,1) == true);
+	CHECK(CheckSorted(arr3,1) == true);
 
 }
 
